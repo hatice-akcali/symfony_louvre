8268c7857a226f2d93508ee2c74d8d41e90e67f6
Hatice Akcali <akcali.hatice68@gmail.com>
2018-01-02 09:33:36 +0100

    Debut des services

---
+++ src/LO/PlatformBundle/Services/CalculBilletTwigExtension.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 01/01/2018
+ * Time: 15:02
+ */
+
+namespace LO\PlatformBundle\Services;
+
+
+class CalculBilletTwigExtension extends \Twig_Extension
+{
+
+    private $calculBilletService;
+
+    public function __construct(\LO\PlatformBundle\Services\CalculBillet $calculBilletService  )
+    {
+        $this->calculBilletService = $calculBilletService;
+
+    }
+
+
+    public function getFilters()
+    {
+        return array(
+            new \Twig_SimpleFilter("calculPrix", array($this, "calculPrix")),
+            new \Twig_SimpleFilter("age", array($this, "calculAge"))
+
+        );
+    }
+
+    /* exemple avec les fonctions
+     * public function getFunctions(){
+        return array(
+            new \Twig_SimpleFunction("getPrenom", array($this, "getPrenom"))
+        );
+    }
+
+    public function getPrenom($prenom){
+        return $prenom;
+    }
+    */
+
+
+    public function calculPrix($billet){
+        $prixBillet = $this->calculBilletService->calculerBillet($billet);
+        return $prixBillet."€";
+    }
+
+
+    public function calculAge($billet){
+
+    }
+}
+
